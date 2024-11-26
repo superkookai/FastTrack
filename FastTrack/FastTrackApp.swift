5b33bc36a3002a7e914dc0c5999759795d069271
Weerawut <weerawut@live.com>
2024-11-26 09:02:33 +0700

    Initial Commit

---
+++ FastTrack/FastTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastTrackApp.swift
+//  FastTrack
+//
+//  Created by Weerawut Chaiyasomboon on 26/11/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct FastTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

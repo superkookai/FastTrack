cc2b47b177bb7edb94f2e47a16eee7b0089dfca4
Weerawut <weerawut@live.com>
2024-11-26 17:01:41 +0700

    Add iOS Target to the App.

---
+++ FastTrackIOS/FastTrackIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastTrackIOSApp.swift
+//  FastTrackIOS
+//
+//  Created by Weerawut Chaiyasomboon on 26/11/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct FastTrackIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
